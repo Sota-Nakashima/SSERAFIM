8aff314578726658decf884337f93bd6fe7c131f
Nakashima Sota <100900504+Sota-Nakashima@users.noreply.github.com>
2023-01-30 14:23:58 +0900

    2023/1/30 14:23

--- script/archive.sh
+++ script/archive.sh
@@ -7,9 +7,11 @@ PALARREL=$3
 mkdir "$OUTPUT_DIR/law_data"
 
 mv $OUTPUT_DIR/bam $OUTPUT_DIR/fastaq $OUTPUT_DIR/fastaq_trim \
-$OUTPUT_DIR/fastqc $OUTPUT_DIR/multiqc $OUTPUT_DIR/reference_genome \
+$OUTPUT_DIR/fastqc $OUTPUT_DIR/reference_genome \
 $OUTPUT_DIR/sam $OUTPUT_DIR/sra \
 -t $OUTPUT_DIR/law_data
+
+mv $OUTPUT_DIR/multiqc -t $OUTPUT_DIR/result
 
 source $CONDA_INIT_PATH
 conda activate bio
---
+++ script/fastp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CONDA_INIT_PATH="$1"
+OUTPUT_DIR="$2"
+OUTPUT_DIR_TRIM_FASTQ="$2/fastaq_trim"
+OUTPUT_DIR_HTML="$2/fastaq_trim/html"
+OUTPUT_DIR_JSON="$2/fastaq_trim/json"
+SRA_LIST_PATH="$3"
+PARALLEL=$4
+
+source $CONDA_INIT_PATH
+conda activate bio
+
+mkdir $OUTPUT_DIR_TRIM_FASTQ $OUTPUT_DIR_HTML $OUTPUT_DIR_JSON
+
+#change IFS and input sra quary
+IFS=$'\n'
+file=(`cat "$SRA_LIST_PATH"`)
+#return IFS default
+IFS=$' \t\n'
+
+for line in "${file[@]}"; do
+    fastp "$OUTPUT_DIR/fastaq/${line}.fq.gz" \
+    -o "$OUTPUT_DIR_TRIM_FASTQ/${line}_trimmed.fq.gz" \
+    -h "$OUTPUT_DIR_HTML/${line}.html"
+    -j "$OUTPUT_DIR_JSON/${line}.json"
+    -w "$PARALLEL"
+done
+
